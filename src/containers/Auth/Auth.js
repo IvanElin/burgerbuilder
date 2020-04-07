052e5ac550173c01da243ce22b8d5c24c4966042
Ivan Elin <ivan.nero1@gmail.com>
2020-04-07 16:58:03 +0300

    Added Sign In Form

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    inputChangedHandler = ( event, controlName ) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        }
+        this.setState({controls: updatedControls})
+
+    }
+
+    checkValidity( value, rules ) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        return isValid;
+    }
+
+
+    render() {
+        const formElementsArray = [];
+        for ( let key in this.state.controls ) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            })
+        }
+
+        let form = (
+            <form onSubmit={this.orderConfirmHandler} >
+                {formElementsArray.map(elem => (
+                    <Input 
+                    key={elem.id}
+                    elementType={elem.config.elementType} 
+                    elementConfig={elem.config.elementConfig}
+                    value={elem.config.value} 
+                    changed={(event) => this.inputChangedHandler(event, elem.id)}
+                    invalid={!elem.config.valid}
+                    shouldValidate={elem.config.validation}
+                    touched={elem.config.touched} />
+                ))}
+                
+                {/* <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button> */}
+                <Button btnType="Success">SUBMIT</Button>
+            </form>
+        );
+
+        return (
+            <div className={classes.Auth}>
+                {form}  
+            </div>
+        );
+    }
+};
+
+export default Auth;
