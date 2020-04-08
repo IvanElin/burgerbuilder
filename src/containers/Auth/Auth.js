fbc970a9c749f201a3abfd7fdb5bc035217dfdcd
Ivan Elin <ivan.nero1@gmail.com>
2020-04-08 17:14:49 +0300

    Added-Logging-Out

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import classes from './Auth.module.css';
 import Input from '../../components/UI/Input/Input';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import Button from '../../components/UI/Button/Button';
 import * as actions from '../../store/actions/index';
 
@@ -105,12 +106,24 @@ class Auth extends Component {
                 ))}
 
                 <Button btnType="Success">SUBMIT</Button>
-            </form>
-            
+            </form>  
         );
+
+        if (this.props.loading) {
+            form = <Spinner/>
+        }
+
+        let errorMessage = null;
+
+        if(this.props.error) {
+            errorMessage = (
+            <p>{this.props.error.message}</p>
+            )
+        }
 
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 {form}
             <Button
                 clicked={this.switchAuthMethodHandler} 
@@ -120,10 +133,17 @@ class Auth extends Component {
     }
 };
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: ( email, password, isSignup ) => dispatch(actions.auth( email, password, isSignup ))
     };
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
